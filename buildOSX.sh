d22d8e20ef402541cfb04f00233b40cf3ff2d7b3
Ryan Cheu <ryancheu@mit.edu>
2016-03-27 13:13:38 -0700

    Update OSX build file

--- buildOSX.sh
+++ buildOSX.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LINK="-lIrrlicht -lglfw3"
-SRCS="main.cpp types.cpp"
+SRCS="main.cpp types.cpp util.cpp"
 INC=""
 
 g++ -std=c++11 ${INC} ${SRCS} ${LINK} -o RigidVoxels -framework OpenGL -framework Cocoa -framework IOKit
